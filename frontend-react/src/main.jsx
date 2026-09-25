bcdec419fe647c49a2c8eaee1a07f7a17c28e47e
Ashutosh Singh <ashutoshsinghthakur0827@gmail.com>
2026-09-26 00:26:16 +0530

    Change import paths to include .jsx extension

--- frontend-react/src/main.jsx
+++ frontend-react/src/main.jsx
@@ -1,10 +1,9 @@
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
-import App from "./App";
-import { AppProvider } from "./context/AppContext";
+import App from "./App.jsx";
+import { AppProvider } from "./context/AppContext.jsx";
 
 import "./index.css";
 
